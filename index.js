2f9540f5539d91edde957cf5477c5b3096cf5efa
nickrozbeck <orozbekovnurali@gmail.com>
2022-09-29 16:43:30 +0600

    add classWork

---
+++ index.js
@@ -0,0 +1,119 @@
+
+// TASK 1;
+
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+ // TASK 2;
+
+// let arr2 = [1, 2, 3, 4];
+// result = 0;
+// for (let i = 0; i < arr2.length; i++) {
+//     result += arr2[i];
+// }
+// console.log(result);
+
+
+// TASK 3;
+
+// const obj = {
+//     'Минск': 'Беларусь',
+// 	'Москва': 'Россия',
+//     'Киев': 'Украина'
+// }
+// for (let i in obj) {
+//     console.log(i +"-это " + obj[i])
+// }
+
+
+
+// TASK 4;
+
+// for (let i = 11; i <= 33; i++) {
+//     console.log(i);
+
+// }
+
+
+// TASK 5;
+
+// const obj = {
+//     Коля: 200,
+//     Вася: 300,
+//     Петя: 400,
+// }
+// for (let i in obj) {
+//     console.log(i+"-зарплата "+ obj[i]+ " долларов");
+// }
+
+
+// TASK 6;
+
+// for (let i = 1000; 50 < i; i+0) {
+//     result = i /= 2
+//     console.log(result);
+// }
+
+
+
+// TASK 7;
+
+// const num = 10;
+// if (num === 10) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
+
+
+// TASK 8;
+
+// let min = prompt('Enter a number');
+// if (min >= 0 && min <= 14) {
+//     alert('First quarter');
+// } if (min >= 15 && min <= 29) {
+//     alert('Second quarter');
+// } if (min >= 30 && min <= 44) {
+//     alert('Third quarter');
+// } if (min >= 45 && min <= 59) {
+//     alert('Fourth quarter');
+// }
+
+
+
+// TASK 9;
+
+// let lang = prompt ('Choose your language (ru,en)','');
+// if (lang == 'ru'){
+//     let rus = ['Пн', 'Вт','Ср','Чт','Пт','Сб','Вс'];
+//     alert (rus)
+// } else if (lang == 'en') {
+//     let eng = ['Mon', 'Tue','Wed','Thu','Fri','Sat', 'Sun'];
+//     alert (eng)
+// }
+    
+
+
+// TASK 10;
+
+// let a = prompt('Enter number (1, 0, -3)');
+// if (a === 0) {
+//     alert('Correct number');
+// } else if (a === 1 || a === -3) {
+//     alert('Invalid number');
+// }
+
+
+
+
+// TASK 17;
+
+// let a = '1';
+// if (a === '1') {
+//     console.log('Correct');
+// } else if (a === 1 || a === -3) {
+//     console.log('Wrong');
+// }
